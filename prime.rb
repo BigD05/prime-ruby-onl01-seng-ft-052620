93eb4bdc5c5e8acd4d09d939f63f64e6971a1fc5
kyle Dornan <kyledornan26@gmail.com>
2020-06-02 12:36:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(number)
-  start = 2
+start = 2
   if number > 1
     range = (start..number-1).to_a
     range.none? do |num_to_test| 
-    mnumber % num_to_test == 0
+    number % num_to_test == 0
     end
   else
     false
